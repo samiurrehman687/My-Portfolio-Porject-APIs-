f8868ef567916e8f11bb32afcbab1684b9c6246a
Article Django Site <samiurrehman38631@gmail.com>
2026-04-17 18:00:23 +0500

    fix frontend Problem

--- Portfolio_Frontend/vite.config.js
+++ Portfolio_Frontend/vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(() => {
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base: '/',
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
-      host: false, // Allow access from network devices
+      host: false,
       proxy: {
         "/api": {
           target: "http://localhost:5000",
