cc982eed797d1d0a9636c586d887bd26b91d9199
Article Django Site <samiurrehman38631@gmail.com>
2026-04-17 18:18:27 +0500

    fix frontend router fix

--- Portfolio_Frontend/src/routes/Router.jsx
+++ Portfolio_Frontend/src/routes/Router.jsx
@@ -6,33 +6,23 @@ const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 const UnderConstruction = lazy(() => import("../pages/UnderConstruction"));
 
-const repoName = import.meta.env.VITE_REPO_NAME;
-
-const basename =
-  repoName && repoName.trim() !== ""
-    ? `/${repoName.replace(/^\/+|\/+$/g, "")}`
-    : "";
-
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: (
-        <Suspense fallback={<Loading />}>
-          <Main />
-        </Suspense>
-      ),
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "underconstruction",
-          element: <UnderConstruction />,
-        },
-      ],
-    },
-  ],
-  { basename }
-);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Main />
+      </Suspense>
+    ),
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "underconstruction",
+        element: <UnderConstruction />,
+      },
+    ],
+  },
+]);
