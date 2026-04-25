96d40dec675b0e6e28be82dbee8f79277c1660c2
Article Django Site <samiurrehman38631@gmail.com>
2026-04-25 11:59:49 +0500

    frontend url fix

--- Portfolio_Frontend/src/components/contact/Contact.jsx
+++ Portfolio_Frontend/src/components/contact/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
   const [contactInfo, setContactInfo] = useState(null);
 
   useEffect(() => {
-    fetch("${import.meta.env.VITE_API_URL}/api/contactinfo/")
+    fetch(`${import.meta.env.VITE_API_URL}/api/contactinfo/`)
       .then((res) => res.json())
       .then((data) => setContactInfo(data))
       .catch((err) => console.log(err));
